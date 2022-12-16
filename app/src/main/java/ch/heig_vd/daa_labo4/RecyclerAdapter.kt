10c9a49493d7ff7a52a87d49ce818a07ffb0e938
Webblitchy <55515713+Webblitchy@users.noreply.github.com>
2022-12-16 09:33:24 +0100

    Remove useless dependancies

--- app/src/main/java/ch/heig_vd/daa_labo4/RecyclerAdapter.kt
+++ app/src/main/java/ch/heig_vd/daa_labo4/RecyclerAdapter.kt
@@ -2,14 +2,11 @@ package ch.heig_vd.daa_labo4
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.media.Image
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleCoroutineScope
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -17,10 +14,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.IOException
-import java.io.InputStream
 import java.net.URL
-import java.util.*
-import kotlin.math.roundToInt
 
 /*
  * Authors: Eliott Chytil, Maxim Golay & Lucien Perregaux
